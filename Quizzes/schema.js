cfa64d923f0d7c6f1631ea9ab974f5f91d51a910
Azucarheart Clues <connietliu2020@gmail.com>
2024-04-21 12:50:00 -0400

    schema

---
+++ Quizzes/schema.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+const quizSchema = new mongoose.Schema({
+    quizType: String,
+    points: Number,
+    assignmentGroup: String,
+    shuffleAnswers: Boolean,
+    timeLimit: Number,
+    multipleAttempts: Boolean,
+    showCorrectAnswers: Boolean,
+    accessCode: String,
+    oneQuestionAtTime: Boolean,
+    webcamRequired: Boolean,
+    lockQuestionsAfterAnswering: Boolean,
+    dueDate: Date,
+    availableDate: Date,
+    untilDate: Date,
+    published: Boolean,
+    name: String,
+    course: String,
+    questions: [{
+      questionType: String,
+      title: String,
+      points: Number,
+      question: String, // may be changed after using wysiwyg
+      correctAnswer: mongoose.Mixed,
+      possibleAnswers: [String],
+      correctAnswers: [String],
+      // typing is less strict here - not sure how to do it well in Mongoose
+    }]
+  },
+  { collection: "quizzes" });
+export default quizSchema;
