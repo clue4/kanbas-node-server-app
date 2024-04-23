e468906c4ff61ccf744b2ee30048a0bc4c29ac3a
joshw2048 <66072737+joshw2048@users.noreply.github.com>
2024-04-22 23:05:28 -0400

    structure routes then i got tired (#3)

---
+++ Kanbas/Quizzes/QuestionsEditor/dao.js
@@ -0,0 +1,12 @@
+import model from "./model.js";
+export const createQuestion = (question) => {
+// logic
+};
+
+export const getAllQuestionsForQuiz = (quizId) => model.find({
+    // logic
+});
+
+export const updateQuestion = (quizId, title) =>  model.updateOne({
+// logic
+});
---
+++ Kanbas/Quizzes/QuestionsEditor/schema.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const questionSchema = new mongoose.Schema({
+  type: { type: String, required: true, enum: ['MultipleChoice', 'TrueFalse', 'FillInBlank'] },
+  title: { type: String, required: true },
+  points: { type: Number, required: true },
+  question: { type: String, required: true },
+  correctAnswer: { type: mongoose.Schema.Types.Mixed, required: function() {
+    return this.type === 'MultipleChoice' || this.type === 'TrueFalse';
+  }},
+  possibleAnswers: { type: [String], required: function() {
+    return this.type === 'MultipleChoice';
+  }},
+  correctAnswers: { type: [String], required: function() {
+    return this.type === 'FillInBlank';
+  }},
+}, 
+{ collection: "quizzes" });
+
+export default questionSchema;
