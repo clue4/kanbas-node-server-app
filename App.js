12d2d8e64adc39568a87429b8b0c1172ceeab218
joshw2048 <joshw2048@gmail.com>
2024-04-24 01:31:57 -0400

    endpoint, WYSIWYG, remove questions from quiz type
    - endpoints to get questions for a quiz and save questions for a quiz is working with mongo
    - WSIWYG question content editor is working
    - quiz no longer has questions[] field, added in a numQuestions field instead

--- App.js
+++ App.js
@@ -9,7 +9,7 @@ import CourseRoutes from "./Kanbas/courses/routes.js"
 import ModuleRoutes from "./Kanbas/modules/routes.js"
 import Hello from "./hello.js";
 import Lab5 from "./lab5.js";
-import QuizQuestionsRoutes from "./Kanbas/Quizzes/QuestionsEditor/routes.js";
+import QuizQuestionsRoutes from "./Quizzes/QuestionsEditor/routes.js";
 
 const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas'
 const DB_NAME = process.env.DB_NAME;
--- Quizzes/QuestionsEditor/routes.js
+++ Quizzes/QuestionsEditor/routes.js
@@ -2,14 +2,46 @@ import * as dao from "./dao.js";
 
 // gonna use database/api/courses/ as base
 function QuizQuestionsRoutes(app) {
-  app.get("/:cid/quizzes/:qid/questions", async (req, res) => {
-    // logic
+  // gets all questions for a quiz
+  app.get("/api/courses/quizzes/:qid/questions", async (req, res) => {
+    const { qid } = req.params; 
+    const allQuestions = await dao.getAllQuestionsForQuiz(qid); 
+    let totalPoints = 0; 
+
+    for (const question of allQuestions) {
+        totalPoints = totalPoints + question.points;
+    }
+    res.json({questions: allQuestions, numQuestions: allQuestions.length, totalPoints: totalPoints}); 
   });
-  app.post("/:cid/quizzes/:qid/questions", async (req, res) => {
-    // logic
+
+  // posts all questions to database when a quiz's editing session is done
+  app.post("/api/courses/quizzes/:qid/questions", async (req, res) => {
+    const { qid } = req.params;
+    const questions = req.body;
+    const newQuestions = [];
+    console.log(qid);
+  
+    try {
+      await dao.deleteAllQuestionsForQuiz(qid);
+      console.log(dao.getAllQuestionsForQuiz(qid));
+  
+      for (const question of questions) {
+        try {
+          await dao.createQuestion(question);
+          newQuestions.push(question);
+        } catch (error) {
+          return res.json({ question, error });
+        }
+      }
+  
+      console.log(dao.getAllQuestionsForQuiz(qid));
+
+      // If all questions were processed successfully, send a success response
+      return res.json({ success: true, newQuestions });
+    } catch (error) {
+      return res.json({ error });
+    }
   });
-  app.put("/:cid/quizzes/:cid/questions", async (req, res) => {
-    // logic
-  });
+
 }
 export default QuizQuestionsRoutes;
