cf73f9971689a8d6e1689113255af91b955139b3
Azucarheart Clues <connietliu2020@gmail.com>
2024-04-24 21:53:27 -0400

    fixed bugs

--- Quizzes/dao.js
+++ Quizzes/dao.js
@@ -1,8 +1,10 @@
 import model from "./model.js";
 export const createQuiz = (quiz) => {
-  model.create(quiz);
+  const newlyCreatedQuiz = model.create(quiz);
+  return newlyCreatedQuiz;
 }
 export const findAllQuizzesForCourse = (courseId) => model.find({ course: courseId });
 export const findQuizById = (quizId) => model.findById(quizId);
 export const updateQuiz = (quizId, quiz) =>  model.updateOne({ _id: quizId }, { $set: quiz });
 export const deleteQuiz = (quizId) => model.deleteOne({ _id: quizId });
+export const findAllQuizzes = () => model.find();
